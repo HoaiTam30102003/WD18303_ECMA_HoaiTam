d526822bbdf1721cc60660d8621ccca2f8642d6d
HoaiTam30102003 <hoaitam30102003@gmail.com>
2024-01-13 23:01:22 +0700

    Bài 4

---
+++ Lab3/Bai4.js
@@ -0,0 +1,15 @@
+let convertTemperature = (temperature, unit) => {
+    if(unit === "C"){
+        let c = (temperature * 9/5) + 32;
+        return c;
+    }else if(unit === "F"){
+        let f = (temperature - 32) * 5/9;
+        return f;
+    }else{
+        let err = "Vui lòng điền C hoặc F !!!";
+        return err;
+    }
+}
+console.log(convertTemperature(30, "C"));
+console.log(convertTemperature(30, "F"));
+console.log(convertTemperature(30, "R"))
